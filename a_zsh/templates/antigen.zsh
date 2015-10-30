9a3569c9e32bf9ddbbcb19a0c3488fbeab36404b
peteches <peteches@yog-sothoth.peteches.co.uk>
2015-10-30 16:55:10 +0000

    Updated antigen to use work plugin system too, added ack as dep for zsh

--- a_zsh/templates/antigen.zsh
+++ a_zsh/templates/antigen.zsh
@@ -3,10 +3,7 @@ source {{zsh_antigen_dir}}/antigen.zsh
 antigen theme $( shuf -n2 ~/.zsh_favlist )
 antigen use oh-my-zsh
 antigen bundles <<EOF
-{%for plugin in zsh_plugins%}
-{{plugin|indent(2, true)}}
-{%endfor%}
-{%for plugin in work_zsh_plugins%}
+{%for plugin in all_zsh_plugins%}
 {{plugin|indent(2, true)}}
 {%endfor%}
 EOF
